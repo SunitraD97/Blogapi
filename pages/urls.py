5e9354122308447e92d95923f26cd51ae64f953d
sunitra <sunitra1525@gmail.com>
2019-03-14 16:51:08 +0700

    update docapi

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     #path('about', views.about, name='about'),
-   
+    path('post', views.post, name="post"),
+    path('get', views.get, name="get"),
+    path('put', views.put, name="put"),
+    path('delete', views.delete, name="delete"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def post(request):
+    return render(request,'pages/doc-post.html')
+
+def get(request):
+    return render(request,'pages/doc-get.html')
+ 
+def put(request):
+    return render(request,'pages/doc-put.html')   
+
+def delete(request):
+    return render(request,'pages/doc-delete.html')
